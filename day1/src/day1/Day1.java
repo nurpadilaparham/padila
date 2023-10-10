ce6e1a4edb4ffa7c965630dce3435559ac7cbd12
nurpadilaparham <nurpadila.parham29@gmail.com>
2023-10-10 14:34:34 +0800

    first commit

---
+++ day1/src/day1/Day1.java
@@ -0,0 +1,27 @@
+package day1;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        String nama = "NURPADILA";
+        String alamat = "SENDANA";
+        String nim = "D0221031";
+        char kelas = 'C';
+        String prodi = "informatika";
+        long angkatan = 2021;
+        double tinggi_badan = 158;
+        float berat_badan = 58;
+        boolean belum_menikah = true;
+        System.out.println("Nama :"+nama);
+        System.out.println("Alamat :"+alamat);
+        System.out.println("Nim :"+nim);
+        System.out.println("Kelas :"+kelas);
+        System.out.println("Prodi :"+prodi);
+        System.out.println("Angkatan :"+angkatan);
+        System.out.println("Tinggi badan :"+tinggi_badan);
+        System.out.println("Berat badan :"+berat_badan);
+        System.out.println("Belum menikah :"+belum_menikah);
+    }
+    
+}
+ 
