54610366ecde8be925b9e8ddb14d9d33d3575acd
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-10-13 22:40:42 +0800

    operasi aritmatika
    operasi aritmatika

---
+++ day5.java
@@ -0,0 +1,23 @@
+package days.of.coding;
+
+public class day5 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int a = 3;
+        int b = 3;
+
+        // Operasi penjumlahan,pengurangan,perkalian,pembagian,sisa bagi
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        int g = a % b;
+        
+        System.out.println("Hasil penjumlahan: " + c);
+        System.out.println("Hasil pengurangan: " + d);
+        System.out.println("Hasil perkalian: " + e);
+        System.out.println("Hasil pembagian: " + f);
+        System.out.println("Hasil sisa bagi: " + g);
+    }
+}
+
