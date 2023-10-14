f909d748a00b3ef3b71091e0d78753079584f337
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-10-14 22:01:04 +0800

    operator
    operator

---
+++ day6.java
@@ -0,0 +1,41 @@
+package days.of.coding;
+
+public class day6 {
+    public static void main(String[] args) {
+        //operator aritmatika penjumlahan
+        System.out.println("Operator aritmatika");
+        int p = 11;
+        int a = 29;
+
+        int c = p + a; 
+        System.out.println("c = " + c);
+        System.out.println("-------------------------------------------------------------------");
+        
+        // Operator penugasan sederhana
+        System.out.println("Operator penugasan");
+        int d = 10;
+
+        d = 23; 
+        System.out.println("d = " + d);
+        System.out.println("-------------------------------------------------------------------");
+        
+        //Operator Perbandingan lebih besar dari
+        System.out.println("Operator perbandingan");
+        int i = 13;
+        int l = 30;
+        
+        boolean n = i > l; 
+        System.out.println("n = " + n);
+        System.out.println("-------------------------------------------------------------------");
+        
+        //Operator logika AND
+        System.out.println("Operator logika");
+        boolean q = true;
+        boolean s = false;
+
+        boolean t = q && s; 
+        System.out.println("t = " + t);
+        System.out.println("-------------------------------------------------------------------");
+
+    }
+}
