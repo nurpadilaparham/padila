67f0f23e0d7d8b2c670c1dbc72fe312dda111617
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-10-15 15:25:07 +0800

    percabangan
    percabangan

---
+++ day7.java
@@ -0,0 +1,16 @@
+package days.of.coding;
+
+public class day7 {
+    public static void main(String[] args) {
+        int nilai = 100;
+
+        if (nilai > 70) {
+            System.out.println("Nilai A");
+        } else if (nilai > 50) {
+            System.out.println("Nilai B");
+        } else {
+            System.out.println("Nilai C");
+        }
+    }
+}
+
