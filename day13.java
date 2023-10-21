3aed9fba7975e98a209cc141703d2b806df247a0
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-10-21 19:33:01 +0800

    for
    for

---
+++ day13.java
@@ -0,0 +1,10 @@
+package days.of.coding;
+
+public class day13 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 1000; i++) {
+            System.out.println("semangat guys");
+        }
+    }
+}
