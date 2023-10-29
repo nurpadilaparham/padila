62eeb4d3a4df8223f64a9abf9c1efa400f8107ea
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-10-29 12:11:59 +0800

    program perulangan mengitung terbesar-terkecil
    program perulangan mengitung terbesar-terkecil

---
+++ day21.java
@@ -0,0 +1,9 @@
+package days.of.coding;
+
+public class day21 {
+    public static void main(String[] args) {
+        for (int i = 10; i >= 1; i--) {
+            System.out.println("Hitungan terbesar ke yang terkecil: " + i);
+        }
+    }
+}
