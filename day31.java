6fd1220deccf83f8281bbd3c51a6d7dc3c9958b5
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-11-08 19:07:03 +0800

    program aritmatika
    program aritmatika

---
+++ day31.java
@@ -0,0 +1,18 @@
+package days.of.coding;
+
+public class day31 {
+    public static void main(String[] args) {
+        int angka1 = 29;
+        int angka2 = 11;
+
+        int hasilPenjumlahan = angka1 + angka2;
+        int hasilPengurangan = angka1 - angka2;
+        int hasilPerkalian = angka1 * angka2;
+        int hasilPembagian = angka1 / angka2;
+
+        System.out.println("Hasil Penjumlahan: " + hasilPenjumlahan);
+        System.out.println("Hasil Pengurangan: " + hasilPengurangan);
+        System.out.println("Hasil Perkalian: " + hasilPerkalian);
+        System.out.println("Hasil Pembagian: " + hasilPembagian);
+    }
+}
