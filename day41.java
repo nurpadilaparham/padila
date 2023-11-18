6cc8689bf293d65c90c12e5b257a2ca407177611
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-11-18 20:40:24 +0800

    program array menyimpan objek
    program array menyimpan objek

---
+++ day41.java
@@ -0,0 +1,12 @@
+package days.of.coding;
+
+public class day41 {
+    public static void main(String[] args) {
+        String[] namaBulan = {"Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"};
+
+        System.out.println("Nama-nama bulan dalam setahun:");
+        for (String bulan : namaBulan) {
+            System.out.println(bulan);
+        }
+    }
+}
