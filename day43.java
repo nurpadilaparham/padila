df7bd60e2383cf549f31c790987bcc5966c1b968
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-11-20 20:20:59 +0800

    perulangan while
    perulangan while

---
+++ day43.java
@@ -0,0 +1,12 @@
+package days.of.coding;
+
+public class day43 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <=1000){
+            System.out.println(i + " Hari ini day43 ");
+            i++;
+        }
+    
+    }
+}
