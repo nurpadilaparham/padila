a0d37d1f58adb591787c29b446176d02a9f92abd
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-11-23 21:49:26 +0800

    program array mencetak dan mengakses nilai array
    program array mencetak dan mengakses nilai array

---
+++ day46.java
@@ -0,0 +1,12 @@
+package days.of.coding;
+
+public class day46 {
+    public static void main(String[] args) {
+        int[] pdl = {29, 30, 31, 32, 33};
+
+        System.out.println("Elemen array:");
+        for (int i = 3; i < pdl.length; i++) {
+            System.out.println(pdl[i]);
+        }
+    }
+}
