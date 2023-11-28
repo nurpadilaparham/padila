0332c450d34f01e5643a0825a542e8a8c507623b
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-11-28 19:41:48 +0800

    mengupdate nilai variabel boolean
    mengupdate nilai variabel boolean

---
+++ day51.java
@@ -0,0 +1,17 @@
+package days.of.coding;
+
+public class day51 {
+    public static void main(String[] args) {
+        // Mendeklarasikan dvariabel boolean dengan nama variabel status
+        boolean status = true;
+
+        // Mencetak nilai awal variabel yaitu true
+        System.out.println("Nilai awal variabel: " + status);
+
+        // Mengupdate atau mengubah nilai pada variabel menjadi false
+        status = false;
+
+        // Mencetak nilai baru yang telah diperbarui
+        System.out.println("Nilai setelah pembaruan: " + status);
+    }
+}
