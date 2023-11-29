eb062ae08547cdc9f7e97adab74a934dd5b946fe
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2023-11-29 20:40:32 +0800

    ternary menentukan nilai maksimum dari dua angka
    ternary menentukan nilai maksimum dari dua angka

---
+++ day52.java
@@ -0,0 +1,17 @@
+package days.of.coding;
+
+public class day52 {
+    public static void main(String[] args) {
+        // Contoh operator ternary untuk menentukan nilai maksimum dari dua angka
+        int num1 = 25;
+        int num2 = 30;
+
+        int max = (num1 > num2) ? num1 : num2;  // Operator ternary: Jika num1 lebih besar dari num2, max diisi dengan num1, jika tidak, diisi dengan num2.
+        System.out.println("Angka maksimum antara " + num1 + " dan " + num2 + " adalah " + max);
+
+        // Contoh operator ternary dengan tipe data string
+        int score = 75;
+        String status = (score >= 60) ? "Lulus" : "Tidak Lulus";  // Operator ternary: Jika score lebih besar atau sama dengan 60, status diisi "Lulus", jika tidak, diisi "Tidak Lulus".
+        System.out.println("Status: " + status);
+    }
+}
