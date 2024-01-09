d8700f122f2007a6b842b03a0ed8a7e536c5c745
nurpadila_d0221031_c <99775650+nurpadilaparham@users.noreply.github.com>
2024-01-09 09:48:43 +0800

    perulangan for mencetak huruf A sampai Z
    perulangan for mencetak huruf A sampai Z

---
+++ day93.java
@@ -0,0 +1,10 @@
+package days.of.coding;
+
+public class day93 {
+    public static void main(String[] args) {
+        //program untuk mencetak huruf a sampai z
+        for (char huruf = 'A'; huruf <= 'Z'; huruf++) {//menggunakan perulangan for dimana nama variabelnya adalah huruf dengan nilai awal A
+            System.out.print(huruf + " ");//perulangan akan terus berlanjut selama huruf lebih kecul atau sama dengan Z, cetak hurup sampai perulangan selesai
+        }
+    }
+}
